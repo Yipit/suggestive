9795c0b32d731de79940d798392be1a18ddd7ba5
Lincoln de Sousa <lincoln@comum.org>
2013-04-17 15:31:02 -0400

    Prototyping new features and improving the API by changing the DummyBackend

--- suggestive/__init__.py
+++ suggestive/__init__.py
@@ -1,12 +1,47 @@
+from __future__ import unicode_literals
+from collections import defaultdict
+from itertools import chain
+
+
+def expand(phrase):
+    """Turns strings like this:
+
+        >>> data = "Lincoln"
+
+    Into this:
+
+        >>> expand(data)
+        ['l', 'li', 'linc', 'linco', 'lincol', 'lincoln']
+    """
+    return list(chain.from_iterable(
+        [word[0:index + 1] for index, sub in enumerate(word)]
+        for word in phrase.lower().split()))
+
+
 class DummyBackend(object):
     def __init__(self):
-        self.data = {}
+        self._documents = {}
+        self._terms = defaultdict(list)
 
-    def index(self, data_source):
-        self.data = data_source
+    def documents(self):
+        return self._documents
+
+    def index(self, data_source, field):
+        count = 0
+        for doc in data_source:
+            doc_id = doc['id']
+            self._documents[doc_id] = doc
+            for term in expand(doc[field]):
+                self._terms[term].append(doc_id)
+            count += 1
+
+        return count
 
     def query(self, term, field, sort):
-        result = filter(lambda x: x[field].startswith(term), self.data)
+        result = []
+        documents = self.documents()
+        for doc_id in self._terms[term.lower()]:
+            result.append(documents[doc_id])
         if sort:
             result.sort(key=lambda x: x[sort])
         return result
@@ -18,8 +53,8 @@ class Suggestive(object):
         self.db = db
         self.backend = backend
 
-    def index(self, data_source, sort_key=None):
-        self.backend.index(data_source)
+    def index(self, data_source, field):
+        self.backend.index(data_source, field)
 
     def suggest(self, term, field, sort=None):
         return self.backend.query(term, field, sort)
--- tests/unit/test_suggestive.py
+++ tests/unit/test_suggestive.py
@@ -1,29 +1,73 @@
 # -*- coding: utf-8; -*-
+from __future__ import unicode_literals
 import suggestive
 
 
-def test_basic_source_answer():
+def test_suggestive():
     s = suggestive.Suggestive('names', backend=suggestive.DummyBackend())
 
     # Given that I have a source registered in my api
     data = [
-        {"name": "Fafá de Belém", "score": 23},
-        {"name": "Fábio Júnior", "score": 12.5},
-        {"name": "Fábio", "score": 20000},
+        {"id": 0, "name": "Fafá de Belém", "score": 23},
+        {"id": 1, "name": "Fábio Júnior", "score": 12.5},
+        {"id": 2, "name": "Fábio", "score": 20000},
     ]
 
     # When I index this source
-    s.index(data)
+    s.index(data, field='name')
 
     # Then I see that I can filter my results using the `term` parameter of the
     # `suggest()` method
     s.suggest('Fa', field='name').should.equal([
-        {"name": "Fafá de Belém", "score": 23},
+        {"id": 0, "name": "Fafá de Belém", "score": 23},
     ])
 
     # And I also see that I can sort my results
     s.suggest('F', field='name', sort='score').should.equal([
-        {"name": "Fábio Júnior", "score": 12.5},
-        {"name": "Fafá de Belém", "score": 23},
-        {"name": "Fábio", "score": 20000},
+        {"id": 1, "name": "Fábio Júnior", "score": 12.5},
+        {"id": 0, "name": "Fafá de Belém", "score": 23},
+        {"id": 2, "name": "Fábio", "score": 20000},
     ])
+
+
+def test_expand():
+    suggestive.expand("Lincoln Clarete").should.equal([
+        'l', 'li', 'lin', 'linc', 'linco', 'lincol', 'lincoln',
+        'c', 'cl', 'cla', 'clar', 'clare', 'claret', 'clarete',
+    ])
+
+
+def test_dummy_backend():
+    # Given that I have an instance of our dummy backend
+    data = [{"id": 0, "name": "Lincoln"}, {"id": 1, "name": "Clarete"}]
+    backend = suggestive.DummyBackend()
+
+    # When I try to index stuff
+    indexed = backend.index(data, field='name')
+
+    # Then I see that the number of indexed items is right
+    indexed.should.equal(2)
+
+    # And that all the documents are indexed
+    backend.documents().should.equal({
+        0: {u'id': 0, u'name': u'Lincoln'},
+        1: {u'id': 1, u'name': u'Clarete'}
+    })
+
+    # And that the cache contains all indexed fields
+    dict(backend._terms).should.equal({
+        'c': [1],
+        'cl': [1],
+        'cla': [1],
+        'clar': [1],
+        'clare': [1],
+        'claret': [1],
+        'clarete': [1],
+        'l': [0],
+        'li': [0],
+        'lin': [0],
+        'linc': [0],
+        'linco': [0],
+        'lincol': [0],
+        'lincoln': [0],
+    })
